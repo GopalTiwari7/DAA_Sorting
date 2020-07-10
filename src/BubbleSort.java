2d3bce9c1ceea560d99d7ecdb14e2c6dde3aa9aa
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-07-10 15:29:01 +0530

    create array

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+
+    }
 }
