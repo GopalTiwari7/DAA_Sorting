7d9423619c63374ccdcc4c873fbbb49a29c3e2a1
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-07-10 15:30:16 +0530

    input the array elements

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,6 +5,8 @@ public class BubbleSort {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int[] arr = new int[size];
+        for(int i=0;i<size;i++)
+            arr[i] = scanner.nextInt();
 
     }
 }
