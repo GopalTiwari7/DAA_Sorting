fd57011fadfed2ff20a26c767c137ef376999a60
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-07-10 15:36:40 +0530

    sort the array using bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,6 +7,15 @@ public class BubbleSort {
         int[] arr = new int[size];
         for(int i=0;i<size;i++)
             arr[i] = scanner.nextInt();
+        for(int j=0;j<size-1;j++){
+            for(int k=0;k<size-j-1;k++){
+                if(arr[k] > arr[k+1]){
+                    int temp = arr[k];
+                    arr[k] = arr[k+1];
+                    arr[k+1] = temp;
+                }
+            }
+        }
 
     }
 }
